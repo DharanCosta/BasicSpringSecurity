d905d4ccfe5c36b4d4aafc9e18bc6c10d9ff2ff0
unknown <dharancosta@gmail.com>
2022-07-06 19:59:59 -0300

    Prototype of basic Spring Security system with: Model User, Controller, Repository, Security configuration and UserDetailService

---
+++ src/main/java/com/springsecurityexample/examplespringsecurity/ExamplespringsecurityApplication.java
@@ -0,0 +1,15 @@
+package com.springsecurityexample.examplespringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class ExamplespringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplespringsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurityexample/examplespringsecurity/ExamplespringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurityexample.examplespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplespringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
